dde587d62890f0f47eab24da32b675999053ca31
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-11 11:09:15 -0400

    ye

--- src/main/java/killercreepr/cruxabyss/structure/ActiveAbyssOutpost.java
+++ src/main/java/killercreepr/cruxabyss/structure/ActiveAbyssOutpost.java
@@ -1,26 +1,44 @@
 package killercreepr.cruxabyss.structure;
 
+import com.destroystokyo.paper.ParticleBuilder;
 import killercreepr.cruxstructures.structure.active.SimpleActiveStructure;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.Particle;
 import org.jetbrains.annotations.NotNull;
 
 public class ActiveAbyssOutpost extends SimpleActiveStructure {
-    protected final @NotNull AbyssOutpost stored;
-    public ActiveAbyssOutpost(@NotNull AbyssOutpost stored, @NotNull Chunk chunk) {
-        super(stored, chunk);
-        this.stored = stored;
+    protected final @NotNull AbyssOutpost data;
+    public ActiveAbyssOutpost(@NotNull AbyssOutpost data, @NotNull Chunk chunk) {
+        super(data, chunk);
+        this.data = data;
     }
 
     @Override
     public void tick() {
         super.tick();
-        stored.setLifeSpan(stored.getLifeSpan() - 1);
-        Bukkit.broadcastMessage(stored.getLifeSpan() + "");
+        //data.setLifeSpan(data.getLifeSpan() - 1);
+        Bukkit.broadcastMessage(data.getLifeSpan() + "");
+        new CreateRectangle(center.getWorld(), data.getBoundingBox(), true, true, .5D)
+            .getLocations().forEach(l ->{
+                new ParticleBuilder(Particle.HAPPY_VILLAGER).location(l).spawn();
+            });
     }
 
     @Override
     public boolean shouldStop() {
-        return stored.getLifeSpan() < 1;
+        return data.getLifeSpan() < 1;
+    }
+
+    @Override
+    public void started() {
+        super.started();
+        Bukkit.broadcastMessage("ABYSS OUTPOST STARTED");
+    }
+
+    @Override
+    public void stopped() {
+        super.stopped();
+        Bukkit.broadcastMessage("ABYSS OUTPOST STOPPED");
     }
 }
