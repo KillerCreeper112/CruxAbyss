b8a0dc5cc992bf33505621415200eec2e452faa9
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-08-27 22:26:05 -0400

    ye

--- src/main/java/killercreepr/cruxabyss/persistence/AbyssPersist.java
+++ src/main/java/killercreepr/cruxabyss/persistence/AbyssPersist.java
@@ -1,4 +1,10 @@
 package killercreepr.cruxabyss.persistence;
 
+import killercreepr.crux.persistence.PersistTag;
+import org.bukkit.persistence.PersistentDataType;
+
 public class AbyssPersist {
+    public static final PersistTag<Integer> REFLECTED_TIMES = PersistTag.register(new PersistTag<>(
+        PersistentDataType.INTEGER, "reflected_times"
+    ));
 }
