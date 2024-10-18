7d5396602de2d03ddde17148b5ca72ce878a671f
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-10-17 23:42:03 -0400

    ye

--- src/main/java/killercreepr/cruxabyss/block/active/ActiveAbyssEntitySpawner.java
+++ src/main/java/killercreepr/cruxabyss/block/active/ActiveAbyssEntitySpawner.java
@@ -1,9 +1,9 @@
 package killercreepr.cruxabyss.block.active;
 
 import killercreepr.cruxblocks.block.CruxBlock;
+import killercreepr.cruxblocks.block.flag.BlockBreakFlags;
 import killercreepr.cruxblocks.block.standard.active.ActiveEntitySpawner;
 import killercreepr.cruxblocks.block.standard.component.EntitySpawnerComponent;
-import killercreepr.cruxblocks.user.Miner;
 import killercreepr.cruxworlds.world.entity.NaturalEntitySpawner;
 import org.bukkit.block.Block;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +19,6 @@ public class ActiveAbyssEntitySpawner extends ActiveEntitySpawner {
     public void navigateSpawner() {
         super.navigateSpawner();
         if(delay >= 0) return;
-        breakBlock((Miner) null);
+        breakBlock(null, BlockBreakFlags.empty());
     }
 }
