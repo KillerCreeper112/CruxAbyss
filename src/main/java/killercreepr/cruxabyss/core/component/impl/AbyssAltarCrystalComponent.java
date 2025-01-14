5c14a95546bcc28656fc62661aaa8b63ed0b975f
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-01-14 10:40:46 -0500

    ye

--- src/main/java/killercreepr/cruxabyss/core/component/impl/AbyssAltarCrystalComponent.java
+++ src/main/java/killercreepr/cruxabyss/core/component/impl/AbyssAltarCrystalComponent.java
@@ -1,7 +1,11 @@
 package killercreepr.cruxabyss.core.component.impl;
 
+import com.destroystokyo.paper.ParticleBuilder;
 import killercreepr.crux.api.communication.CreateSound;
 import killercreepr.crux.api.item.CruxItem;
+import killercreepr.crux.core.Crux;
+import killercreepr.crux.core.util.CruxBlockUtil;
+import killercreepr.crux.core.util.CruxMath;
 import killercreepr.cruxabyss.api.altar.AbyssAltar;
 import killercreepr.cruxabyss.api.component.AbyssAltarCrystal;
 import killercreepr.cruxabyss.core.entity.mob.AbyssMob;
@@ -9,11 +13,9 @@ import killercreepr.cruxabyss.core.world.AbyssWorldTypes;
 import killercreepr.cruxcore.CruxCore;
 import killercreepr.cruxworlds.api.world.CruxWorld;
 import killercreepr.cruxworlds.core.component.CruxWorldsComponents;
-import org.bukkit.Color;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -43,7 +45,16 @@ public class AbyssAltarCrystalComponent implements AbyssAltarCrystal {
         Block b = altar.center();
         CruxWorld world = CruxCore.inst().worldManager().getWorld(b.getWorld().getUID());
         if(world != null && AbyssWorldTypes.ABYSS.compare(world.get(CruxWorldsComponents.WORLD_TYPE))){
-            b.getWorld().createExplosion(b.getLocation().toCenterLocation(), 4f, true, true);
+            BlockData data = b.getBlockData();
+            Crux.handlers().block().setType(b, Material.AIR);
+            new ParticleBuilder(Particle.BLOCK)
+                .location(b.getLocation().toCenterLocation())
+                .offset(.1, .1, .1)
+                .extra(.5)
+                .count(CruxMath.random(10, 15))
+                .data(data)
+                .spawn();
+            b.getWorld().createExplosion(b.getLocation().toCenterLocation().add(0, .5, 0), 4f, true, true);
             return;
         }
 
