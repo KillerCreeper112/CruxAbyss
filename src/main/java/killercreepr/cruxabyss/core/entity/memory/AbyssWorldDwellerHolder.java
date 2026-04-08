c6be5874788e58356aea9a0df44cd0f6ada04e85
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2026-04-07 20:08:21 -0400

    ye

--- src/main/java/killercreepr/cruxabyss/core/entity/memory/AbyssWorldDwellerHolder.java
+++ src/main/java/killercreepr/cruxabyss/core/entity/memory/AbyssWorldDwellerHolder.java
@@ -1,25 +1,27 @@
 package killercreepr.cruxabyss.core.entity.memory;
 
-import killercreepr.crux.api.data.Loadable;
 import killercreepr.crux.api.entity.memory.EntityMemory;
 import killercreepr.crux.api.entity.memory.PlayerMemory;
 import killercreepr.crux.core.Crux;
 import killercreepr.crux.core.entity.memory.PlayerTickedDataHolder;
-import killercreepr.crux.core.util.CruxTag;
 import killercreepr.cruxabyss.api.event.PlayerSurvive1MinuteInAbyssEvent;
+import killercreepr.cruxabyss.core.statistic.AbyssStatistic;
 import killercreepr.cruxabyss.core.world.AbyssWorldTypes;
 import killercreepr.cruxcore.CruxCore;
+import killercreepr.cruxstatistics.api.bukkit.BukkitStatisticHolder;
+import killercreepr.cruxstatistics.api.statistic.CruxStatisticHolder;
 import killercreepr.cruxworlds.api.world.CruxWorld;
 import killercreepr.cruxworlds.core.component.CruxWorldsComponents;
 import net.kyori.adventure.key.Key;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class AbyssWorldDwellerHolder extends PlayerTickedDataHolder implements Loadable {
+import java.time.Duration;
+
+public class AbyssWorldDwellerHolder extends PlayerTickedDataHolder {
     public static AbyssWorldDwellerHolder abyssWorldDweller(Player p){
         if(!isAbyss(p.getWorld())) return null;
         return EntityMemory.getOrCreateDataHolder(p, AbyssWorldDwellerHolder.class, mem ->{
@@ -29,9 +31,9 @@ public class AbyssWorldDwellerHolder extends PlayerTickedDataHolder implements L
     }
 
     public static int getAbyssDwellTicks(Player p){
-        AbyssWorldDwellerHolder holder = abyssWorldDweller(p);
+        var holder = BukkitStatisticHolder.statisticHolder(p);
         if(holder == null) return 0;
-        return holder.getTick();
+        return holder.getStatistic(AbyssStatistic.CURRENT_ABYSS_SURVIVE_SECONDS) * 20;
     }
 
     public static boolean isAbyss(World world){
@@ -43,7 +45,7 @@ public class AbyssWorldDwellerHolder extends PlayerTickedDataHolder implements L
     public static final Key KEY = Crux.key("abyss_world_dweller");
     public AbyssWorldDwellerHolder(@NotNull Key key, @NotNull PlayerMemory parent) {
         super(key, parent);
-        load();
+        //load();
     }
     public AbyssWorldDwellerHolder(@NotNull PlayerMemory parent) {
         this(KEY, parent);
@@ -51,39 +53,46 @@ public class AbyssWorldDwellerHolder extends PlayerTickedDataHolder implements L
 
     @Override
     public boolean shouldRemoveFromMemory(@Nullable Player e) {
-        return super.shouldRemoveFromMemory(e) || e == null || !e.isValid() || !isAbyss(e.getWorld());
+        return super.shouldRemoveFromMemory(e) || e == null || !e.isValid() || !isAbyss(e.getWorld()) ||
+        e.getIdleDuration().compareTo(Duration.ofMinutes(1)) >= 0;
     }
 
-    public int getTick() {
-        return tick;
+    @Override
+    protected void removingFromMemory(@Nullable Player e) {
+        super.removingFromMemory(e);
+        if(cacheHolder != null){
+            cacheHolder.setStatistic(AbyssStatistic.CURRENT_ABYSS_SURVIVE_SECONDS, 0);
+        }
     }
 
     @Override
     public void onMemoryUnload(@NotNull Entity e) {
         super.onMemoryUnload(e);
-        save();
+        //save();
     }
 
-    protected int tick = 0;
-
+    protected CruxStatisticHolder cacheHolder;
+    protected byte tick = 0;
     @Override
     protected void onTick(@NotNull Player e) {
         super.onTick(e);
         tick++;
+        //tick every 1 second
+        if(tick % 20 != 0) return;
 
-        if(tick % 1200 == 0){
+        if(cacheHolder == null){
+            cacheHolder = BukkitStatisticHolder.statisticHolder(e);
+            if(cacheHolder == null) return;
+        }
+
+        cacheHolder.incrementStatistic(AbyssStatistic.ABYSS_SURVIVE_SECONDS, 1);
+        cacheHolder.incrementStatistic(AbyssStatistic.CURRENT_ABYSS_SURVIVE_SECONDS, 1);
+
+        var tick = cacheHolder.getStatistic(AbyssStatistic.CURRENT_ABYSS_SURVIVE_SECONDS);
+
+        if(tick % 60 == 0){
             PlayerSurvive1MinuteInAbyssEvent event = new PlayerSurvive1MinuteInAbyssEvent(e);
             event.callEvent();
         }
-    }
-
-    @Override
-    public void save() {
-        CruxTag.set(parent.value(), "abyss_world_dwell_ticks", PersistentDataType.INTEGER, tick);
-    }
-
-    @Override
-    public void load() {
-        tick = CruxTag.get(parent.value(), "abyss_world_dwell_ticks", PersistentDataType.INTEGER, tick);
     }
 }
--- src/main/java/killercreepr/cruxabyss/core/statistic/AbyssStatistic.java
+++ src/main/java/killercreepr/cruxabyss/core/statistic/AbyssStatistic.java
@@ -9,6 +9,8 @@ public class AbyssStatistic {
 
     public static final CruxStatisticType<?> ABYSS_TRAVEL = register(CruxStatisticType.statisticType(Crux.key("abyss_travel")));
     public static final CruxStatisticType<?> ABYSS_OUTPOSTS_CAPTURED = register(CruxStatisticType.statisticType(Crux.key("abyss_outposts_captured")));
+    public static final CruxStatisticType<?> ABYSS_SURVIVE_SECONDS = register(CruxStatisticType.statisticType(Crux.key("abyss_survive_seconds")));
+    public static final CruxStatisticType<?> CURRENT_ABYSS_SURVIVE_SECONDS = register(CruxStatisticType.statisticType(Crux.key("current_abyss_survive_seconds")));
 
     private static CruxStatisticType<?> register(CruxStatisticType<?> type){
         return CruxStatisticRegistries.STATISTIC_TYPE.register(type);
